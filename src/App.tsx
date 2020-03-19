e4bdf6de50cc2daf98a609f77767b3cd578babb2
탁동혁 <tdh8316@naver.com>
2020-03-20 00:10:58 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { AboutPage, HomePage, ProjectsPage } from './pages'
+import logo from './logo.svg';
 import './App.css';
-import { Route, Redirect, Switch } from 'react-router-dom';
 
-
-const App: React.FC = () => {
+function App() {
   return (
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/projects" component={ProjectsPage} />
-
-        <Route component={() => <Redirect to="/" />} />
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as AboutPage } from './About';
-export { default as HomePage } from './Home';
-export {default as ProjectsPage} from './Projects';
