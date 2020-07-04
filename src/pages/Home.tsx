399d55152fac5f6023a74bd08584b5a532c9101a
탁동혁 <tdh8316@naver.com>
2020-07-04 16:57:38 +0900

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { Motion, spring } from "react-motion";
 import { StickyContainer, Sticky } from "react-sticky";
 import "bootstrap/dist/css/bootstrap.css";
-import { Nav, Navbar, Form } from "react-bootstrap";
+import { Nav, Navbar, Form, Row, Col } from "react-bootstrap";
 import "../index.css";
 import "../App.css";
+import { Button } from "react-bootstrap";
 
 const menuStyle: CSS.Properties = {
   marginTop: "-16px"
@@ -23,9 +24,7 @@ const arrowButtonStyle: CSS.Properties = {
   cursor: "pointer"
 };
 const intro: String =
-  "is a student at the day and " +
-  "is a software developer at night. He is passionate about " +
-  "building software that makes our life beautiful.";
+  "He is a student during the day and a programmer at night.";
 
 class HomePage extends React.Component {
   state = {
@@ -64,61 +63,73 @@ class HomePage extends React.Component {
           )}
         </Sticky>
         <header className="App-header" style={headerStyle}>
-          <div className="App-header-intro">
-            <Motion
-              style={{ x: spring(0), opacity: spring(1) }}
-              defaultStyle={{ x: -256, opacity: 0 }}
-            >
-              {({ x, opacity }) => (
-                <div
-                  style={{
-                    WebkitTransform: `translateX(${x}px)`,
-                    transform: `translateX(${x}px)`,
-                    opacity: opacity
-                  }}
+          <Row>
+            <Col>
+              <div className="App-header-intro">
+                <Motion
+                  style={{ x: spring(0), opacity: spring(1) }}
+                  defaultStyle={{ x: -256, opacity: 0 }}
                 >
-                  <h1
-                    style={{
-                      letterSpacing: 1.5,
-                      fontSize: "2.5em",
-                      fontWeight: 400
-                    }}
-                    className="App-header-intro-name"
-                  >
-                    Donghyeok Tak
+                  {({ x, opacity }) => (
+                    <div
+                      style={{
+                        WebkitTransform: `translateX(${x}px)`,
+                        transform: `translateX(${x}px)`,
+                        opacity: opacity
+                      }}
+                    >
+                      <h1
+                        style={{
+                          letterSpacing: 1.5,
+                          fontSize: "2.5em",
+                          fontWeight: 400
+                        }}
+                        className="App-header-intro-name"
+                      >
+                        Donghyeok Tak
                   </h1>
-                </div>
-              )}
-            </Motion>
-            <Motion
-              style={{
-                y: spring(isIntroShow ? 0 : -128),
-                opacity: spring(isIntroShow ? 1 : 0)
-              }}
-              defaultStyle={{ y: -128, opacity: 0 }}
-            >
-              {({ y, opacity }) => (
-                <div
+                    </div>
+                  )}
+                </Motion>
+                <Motion
                   style={{
-                    WebkitTransform: `translate3d(0, ${-y}px, 0)`,
-                    transform: `translate3d(0, ${-y}px, 0)`,
-                    opacity: opacity
+                    y: spring(isIntroShow ? 0 : -128),
+                    opacity: spring(isIntroShow ? 1 : 0)
                   }}
+                  defaultStyle={{ y: -128, opacity: 0 }}
                 >
-                  <h1
-                    style={{
-                      color: "#696969",
-                      fontWeight: 300,
-                      letterSpacing: 1.25,
-                      fontSize: "2em"
-                    }}
-                  >
-                    {intro}
-                  </h1>
-                </div>
-              )}
-            </Motion>
-          </div>
+                  {({ y, opacity }) => (
+                    <div
+                      style={{
+                        WebkitTransform: `translate3d(0, ${-y}px, 0)`,
+                        transform: `translate3d(0, ${-y}px, 0)`,
+                        opacity: opacity
+                      }}
+                    >
+                      <h1
+                        style={{
+                          color: "#696969",
+                          fontWeight: 300,
+                          letterSpacing: 1.25,
+                          fontSize: "1.5em",
+                          paddingBottom: "32px"
+                        }}
+                      >
+                        {intro}
+                      </h1>
+                      <img src="/assets/01.png" alt="" height="360px" className="image1" />
+                    </div>
+                  )}
+                </Motion>
+              </div>
+            </Col>
+            <Col style={{ paddingLeft: "32px" }}>
+              <div><h3>Follow me!</h3></div>
+              <div><Button variant="link" href="https://github.com/tdh8316">Github</Button></div>
+              <div><Button variant="link" href="https://www.instagram.com/do_odoong_tak/">Instagram</Button></div>
+              <div><Button variant="link" href="mailto://tdh8316@naver.com">Email</Button></div>
+            </Col>
+          </Row>
         </header>
         <Motion
           style={{
@@ -155,7 +166,85 @@ class HomePage extends React.Component {
             </div>
           )}
         </Motion>
-        <h1 style={{ textAlign: "center", padding: "32px" }}>Coming soon!</h1>
+
+        <Row>
+          <Col style={{ paddingTop: "5vh", paddingLeft: "15vw", paddingBottom: "25vh" }} className="image2">
+            <img src="/assets/07.png" alt="" width="75%"></img>
+            <h1>Nice to meet you!</h1>
+            <h5 style={{ paddingRight: "64px", fontWeight: 100}}>
+              I enjoy programming and playing the piano. People often think those who interested in science are not that emotional and artistic. I also did. But whenever I play the piano, I would like to break this stereotype up. I'm interested in not only computer science but musical and artistic things.
+              Liberal arts and science, It would be great if we can make our life more beautiful with these!
+            </h5>
+          </Col>
+          <Col>
+            <div>
+              <Motion
+                style={{
+                  y: spring(isIntroShow ? 0 : -128),
+                  opacity: spring(isIntroShow ? 1 : 0)
+                }}
+                defaultStyle={{ y: -128, opacity: 0 }}
+              >
+                {({ y, opacity }) => (
+                  <div
+                    style={{
+                      WebkitTransform: `translate3d(0, ${-y}px, 0)`,
+                      transform: `translate3d(0, ${-y}px, 0)`,
+                      opacity: opacity
+                    }}
+                  >
+                    <h1 style={{ fontSize: "calc(30px + 2vmin)" }}>_CV</h1>
+                  </div>
+                )}
+              </Motion>
+            </div>
+            <h1>Education</h1>
+          </Col>
+
+          <Col style={{ textAlign: "center" }}>
+            <div>
+              <Motion
+                style={{
+                  y: spring(isIntroShow ? 0 : -128),
+                  opacity: spring(isIntroShow ? 1 : 0)
+                }}
+                defaultStyle={{ y: -128, opacity: 0 }}
+              >
+                {({ y, opacity }) => (
+                  <div
+                    style={{
+                      WebkitTransform: `translate3d(0, ${-y}px, 0)`,
+                      transform: `translate3d(0, ${-y}px, 0)`,
+                      opacity: opacity
+                    }}
+                  >
+                    <h1 style={{ fontSize: "calc(25px + 2vmin)" }}>Skills</h1>
+                  </div>
+                )}
+              </Motion>
+            </div>
+            <h2>Languages</h2>
+            <h5>Python</h5>
+            <h5>Go</h5>
+            <h5>Rust</h5>
+            <h5>Kotlin</h5>
+            <h5>Dart</h5>
+            <h5>C/C++</h5>
+            <h5>DotNet</h5>
+            <h5>Typescript</h5>
+            <h2>Frameworks and Libraries</h2>
+            <h5>Flutter</h5>
+            <h5>Qt</h5>
+            <h5>LLVM</h5>
+            <h5>React</h5>
+            <h2>Platforms</h2>
+            <h5>Mobile</h5>
+            <h5>Web</h5>
+            <h5>Desktop</h5>
+            <h5>Arduino</h5>
+          </Col>
+
+        </Row>
       </StickyContainer>
     );
   }
